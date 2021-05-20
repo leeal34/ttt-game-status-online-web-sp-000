7b1a8b2518e6fd5d5085f54c3c871026073e5b8e
Alexander Lee <leeal34uw@gmail.com>
2021-05-20 01:23:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,16 @@ WIN_COMBINATIONS = [
 [0,4,8], # left_diagonal 
 [6,4,2] # right_diagonal 
 ]
+
+def won?(board)
+ WIN_COMBINATIONS.each do |win_combination|
+   win_index_1 = win_combination[0]
+   win_index_2 = win_combination[1]
+   win_index_3 = win_combination[2]
+   position_1 = board[win_index_1] # value of board at win_index_1
+   position_2 = board[win_index_2] # value of board at win_index_2
+   position_3 = board[win_index_3] # value of board at win_index_3
+   position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+ end
+end
+
